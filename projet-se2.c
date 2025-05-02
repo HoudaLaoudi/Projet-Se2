c4f5ac100716c4b9f972d60ba9fd809fb90ba710
HoudaLaoudi <houda.laoudi191004@gmail.com>
2025-05-02 21:32:28 +0100

    projet-se2.c

--- projet-se2.c
+++ projet-se2.c
@@ -11,14 +11,15 @@
 
 sem_t sem_mutex;
 
-int nb_bus_tunnel = 0;     // Nombre de bus dans le tunnel
-int sens_tunnel = -1;      // -1 = libre, 0 = X->Y, 1 = Y->X
+int nb_bus_tunnel = 0;       // Nombre de bus dans le tunnel
+int sens_tunnel = -1;        // -1 = libre, 0 = X->Y, 1 = Y->X
+int dernier_sens_prioritaire = 1; // Pour alterner les priorités
 
 int file_attente_X = 0;
 int file_attente_Y = 0;
 
 void delai_passage() {
-    usleep((rand() % 500 + 1000) * 1000);
+    usleep((rand() % 500 + 1000) * 1000);  // Delai aléatoire entre 1s et 1.5s
 }
 
 void acces_tunnel(int direction) {
@@ -30,13 +31,15 @@ void acces_tunnel(int direction) {
         file_attente_Y++;
 
     while ((nb_bus_tunnel > 0 && sens_tunnel != direction) ||
-           (direction == 0 && file_attente_Y > 0 && sens_tunnel == -1) ||
-           (direction == 1 && file_attente_X > 0 && sens_tunnel == -1)) {
-        sem_post(&sem_mutex);
-        usleep(100000);
-        sem_wait(&sem_mutex);
+           (nb_bus_tunnel == 0 && sens_tunnel == -1 && 
+            ((direction == 0 && file_attente_Y > 0 && dernier_sens_prioritaire == 0) ||
+             (direction == 1 && file_attente_X > 0 && dernier_sens_prioritaire == 1)))) {
+        sem_post(&sem_mutex);  
+        usleep(100000);        
+        sem_wait(&sem_mutex);  
     }
 
+    
     if (direction == 0)
         file_attente_X--;
     else
@@ -44,15 +47,16 @@ void acces_tunnel(int direction) {
 
     nb_bus_tunnel++;
     sens_tunnel = direction;
+    dernier_sens_prioritaire = direction;
 
-    sem_post(&sem_mutex);
+    sem_post(&sem_mutex); 
 }
 
 void sortie_tunnel() {
     sem_wait(&sem_mutex);
     nb_bus_tunnel--;
     if (nb_bus_tunnel == 0) {
-        sens_tunnel = -1;
+        sens_tunnel = -1;  
     }
     sem_post(&sem_mutex);
 }
@@ -63,6 +67,7 @@ void* comportement_bus(void* data) {
     free(data);
 
     for (int i = 1; i <= NB_VOYAGES; i++) {
+        // Aller
         acces_tunnel(origine == 0 ? 0 : 1);
         printf("Bus %d de Ville %c : %c → %c (Aller %d)\n",
                id, origine == 0 ? 'X' : 'Y',
@@ -71,6 +76,7 @@ void* comportement_bus(void* data) {
         delai_passage();
         sortie_tunnel();
 
+        // Retour
         acces_tunnel(origine == 0 ? 1 : 0);
         printf("Bus %d de Ville %c : %c → %c (Retour %d)\n",
                id, origine == 0 ? 'X' : 'Y',
@@ -91,17 +97,19 @@ int main() {
 
     int index = 0;
 
+    //ville X
     for (int i = 0; i < NB_BUS_X; i++) {
         int* infos = malloc(2 * sizeof(int));
         infos[0] = i + 1;
-        infos[1] = 0;
+        infos[1] = 0;  
         pthread_create(&liste_bus[index++], NULL, comportement_bus, infos);
     }
 
+    // Ville Y
     for (int i = 0; i < NB_BUS_Y; i++) {
         int* infos = malloc(2 * sizeof(int));
         infos[0] = i + 1;
-        infos[1] = 1;
+        infos[1] = 1;  
         pthread_create(&liste_bus[index++], NULL, comportement_bus, infos);
     }
 
